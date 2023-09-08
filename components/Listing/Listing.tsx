912956170883bcd38d447c0db277765916f8edbd
Rihards Edels <rihards.edels@magebit.com>
2023-09-08 20:22:47 +0300

    finalize search sort components add favicon

--- components/Listing/Listing.tsx
+++ components/Listing/Listing.tsx
@@ -2,19 +2,22 @@
 
 import React, { useState, useEffect } from "react";
 import Input from "../Input/Input";
-import { mdiClose } from "@mdi/js";
+import { mdiArrowUp, mdiArrowDown, mdiClose } from "@mdi/js";
 
 import classes from "./listing.module.css";
 import ListingCard from "../ListingCard/ListingCard";
 import { Person } from "@/types/types";
+
+import Icon from "@mdi/react";
 
 interface ListingProps {
   listingData: Person[];
 }
 
 const Listing: React.FunctionComponent<ListingProps> = ({ listingData }) => {
-  const [searchQuery, setSearchQuery] = useState(""); // State to store the search query
-  const [filteredData, setFilteredData] = useState(listingData || []);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [filteredData, setFilteredData] = useState<Person[]>(listingData || []);
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc" | "">("");
 
   useEffect(() => {
     const filtered = listingData.filter(
@@ -29,23 +32,53 @@ const Listing: React.FunctionComponent<ListingProps> = ({ listingData }) => {
     setSearchQuery(e.target.value);
   };
 
+  const sortByName = () => {
+    const sortedData = [...filteredData];
+    sortedData.sort((a, b) => {
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+      if (nameA < nameB) {
+        return sortOrder === "asc" ? -1 : 1;
+      }
+      if (nameA > nameB) {
+        return sortOrder === "asc" ? 1 : -1;
+      }
+      return 0;
+    });
+
+    setFilteredData(sortedData);
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
+
   return (
     <div className={classes.root}>
-      <Input
-        onChange={handleClientSearch}
-        value={searchQuery}
-        placeholder={"Search"}
-        icon={mdiClose}
-        action={() => setSearchQuery("")}
-        classes={{ root: classes.inputRoot, inputRoot: classes.input }}
-      />
+      <div className="grid lg:grid-flow-col gap-2 lg:justify-start">
+        <Input
+          onChange={handleClientSearch}
+          value={searchQuery}
+          placeholder={"Search"}
+          icon={searchQuery && mdiClose}
+          action={() => setSearchQuery("")}
+          classes={{ root: classes.inputRoot, inputRoot: classes.input }}
+        />
+        <div className="grid grid-flow-col gap-1 justify-start items-center hover:underline">
+          <span onClick={sortByName} role="button">
+            Sort by name
+          </span>
+          {sortOrder && (
+            <Icon
+              size={"16px"}
+              path={sortOrder === "asc" ? mdiArrowUp : mdiArrowDown}
+            />
+          )}
+        </div>
+      </div>
+
       {searchQuery.length && !filteredData.length ? (
         <h1 className={classes.noResults}>
           Sorry, no matches for your search!
         </h1>
-      ) : (
-        ""
-      )}
+      ) : null}
       <div className={classes.characterGrid}>
         {filteredData.map((person, index) => (
           <ListingCard key={index} characterData={person} />
--- queries/getOnePerson.tsx
+++ queries/getOnePerson.tsx
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const GET_ONE_PERSON = gql`
-  query Person($id: ID!) {
+  query GetOnePerson($id: ID!) {
     person(id: $id) {
       name
       birthYear
